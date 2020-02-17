cba0de555db25372559ab73252b547b07f1bc95e
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-02-17 17:03:38 +0900

    first_commit

---
+++ OutDoor/OutDoor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutDoor
+//
+//  Created by 山根大生 on 2020/02/17.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
