576f96007d124a320fefe74f4943a6922d0873ba
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-02-20 15:22:35 +0900

    SettingViewControllerを追加

---
+++ OutDoor/OutDoor/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  OutDoor
+//
+//  Created by 山根大生 on 2020/02/20.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }    
+}
