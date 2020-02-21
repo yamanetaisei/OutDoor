a7290623a18faede6034b374ee2f2e0f9305e681
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-02-21 18:32:52 +0900

    確認画面で完了ボタンをおすとlabelが切り替わるようにした

--- OutDoor/OutDoor/CheckViewController.swift
+++ OutDoor/OutDoor/CheckViewController.swift
@@ -7,3 +7,22 @@
 //
 
 import UIKit
+
+
+class CheckViewController: UIViewController {
+    
+    @IBOutlet weak var checkItem: UILabel!
+    
+    var pushButton:Int = 0
+    @IBAction func doneButton(_ sender: Any) {
+        pushButton += 1
+        checkItem.text = decideList[pushButton]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //chekLabelの初期値を設定
+        checkItem.text = decideList[0]
+    }
+}
